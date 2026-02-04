ee31b65e20e236cae36e7d8466ff885003e7ff80
Madhav gupta <madhavbhola2552@gmail.com>
2026-02-04 09:56:52 +0530

    all update

---
+++ C14 patterns-II/sathiya.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class sathiya {
+    public static void main(String args[]) {
+		Scanner sc= new Scanner(System.in);
+		int n= sc.nextInt();
+		for(int i=1; i<=n; i++){
+			for(int j=1; j<=n; j++){
+				if(i==(n/2)+1)
+					System.out.print("*");
+				else if(j==(n/2)+1)
+					System.out.print("*");
+				else if(i==1 && j<=(n/2)+1)
+					System.out.print("*");
+				else if(i==n && j<=(n/2)+1)
+					System.out.print("*");
+				else if(j==1 && i>=(n/2)+1)
+					System.out.print("*");
+				else if(j==n && i<=(n/2)+1)
+					System.out.print("*");
+				else
+					System.out.print("");
+			}
+            
+		}
+    }
+}
